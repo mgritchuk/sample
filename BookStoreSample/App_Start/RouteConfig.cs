cbd1d91dfdda934eb9bea6d567a688b2f2638ca0
Kolya <nickthegrave@gmail.com>
2017-08-15 11:53:01 +0300

    removed unnecessary staff from the server app.

--- BookStoreSample/App_Start/RouteConfig.cs
+++ BookStoreSample/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace BookStoreSample
 
 			routes.MapRoute(
 				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				url: "{controller}/{action}/{id}"
+				//defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
